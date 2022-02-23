fa65b8ef0f5ab02b51e512214ea35804fce82f98
reshsingh1995@gmail.com <reshsingh1995@gmail.com>
2022-02-24 00:55:33 +0530

    part time salary calculation

--- empWageCal/src/empWageCal/empWageCal.java
+++ empWageCal/src/empWageCal/empWageCal.java
@@ -2,9 +2,35 @@ package empWageCal;
 
 public class empWageCal {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+	  static int isPresent=1;
+    static int isAbsent=0;
+    static int perHourWage=20;
+    static int wokingHour,partTimeWage=8;
+    static int dailyWage=0;
+    static int isPartTime=2;
+public static void main(String args[])
+{
+    int randomValue = (int)((Math.random()*10) % 3);
+    System.out.println(randomValue);
+    if(randomValue==isPresent){
+            System.out.println("Employee is present");
+            dailyWage=wageCalculationMethod(perHourWage,wokingHour);
+    }
+    if(randomValue==isAbsent){
+            System.out.println("Employee is absent");
+    }
+    if(randomValue==isPartTime) {
+    	System.out.println("Employee is Doing Part Time");
+    	dailyWage=wageCalculationMethod(perHourWage, partTimeWage);
+    }
+    System.out.println(dailyWage);
+}
+
+    public static int wageCalculationMethod(int perDayWage,int workingHour){
+            int dailyWage=perDayWage*workingHour;
+            return dailyWage;
+    }
+
 
 }
